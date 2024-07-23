86a0b63aa7f05725fe30b9296e60e4f712baf2d7
Aydin Joshi <ajoshi1336@gmail.com>
2024-07-23 14:06:44 -0400

    updated commit for me

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -29,7 +29,7 @@ const Clients = () => {
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img
+                {/*<img
                   src={company.img}
                   alt={company.name}
                   className="md:w-10 w-5"
@@ -39,7 +39,7 @@ const Clients = () => {
                   alt={company.name}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
                   className="md:w-24 w-20"
-                />
+                />*/}
               </div>
             </React.Fragment>
           ))}
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "NextJS", "Typescript"];
+  const rightLists = ["HTML5", "CSS3", "TailwindCSS"];
 
   const [copied, setCopied] = useState(false);
 
